fe39bf69bf0f2c51991c9a3955cf44c041810c94
Bonsa11 <sambodza@hotmail.co.uk>
2022-08-22 08:15:29 +0100

    popen -> subprocess

--- check_to_run.py
+++ check_to_run.py
@@ -18,6 +18,7 @@ def run_cleanup(logger):
     try:
         path = get_config().get('SCRIPTS','cleanup')
         cmd = f'sudo sh {path}'
+        logger.info(f'running commmand {cmd}')
         _ = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
     except Exception as e:
         logger.critical(f'failed to run cleanup : {e}')
@@ -27,6 +28,7 @@ def get_new_batch(logger):
     try:
         path = get_config().get('SCRIPTS','batch')
         cmd = f'sudo python3 {path}'
+        logger.info(f'running commmand {cmd}')
         _ = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
     except Exception as e:
         logger.critical(f'failed to get new run : {e}')
